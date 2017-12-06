d0b8653c764b90f58530a6e2f13b4173be28436f
Alex Quispe Roque <new.lex16@gmail.com>
2017-12-06 12:25:59 -0500

    final releases

--- src/app/main.ts
+++ src/app/main.ts
@@ -1,5 +1,9 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {enableProdMode} from "@angular/core";
+import {AppModule} from './app.module';
 
-import { AppModule } from './app.module';
+enableProdMode();
 
-platformBrowserDynamic().bootstrapModule(AppModule);
+platformBrowserDynamic()
+    .bootstrapModule(AppModule)
+    .catch(err => console.error(err));
