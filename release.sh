6dc734884eb49b7bc08bab29b47e891e0934ac70
Alex Christian <alexchristianqr@gmail.com>
2022-07-23 16:01:32 -0500

    [updated] agregado archivo release

---
+++ release.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+# This is the automated release script
+
+# guard against stupid
+if [ -z "$1" ]; then
+   echo "You must specify a new version level: [patch, minor, major]";
+   exit 1;
+fi
+
+tagVersion=$1
+
+set -e
+
+#git tag -d "v$tagVersion"
+git tag -a -m "new tag release v$tagVersion" "v$tagVersion"
+git push origin "v$tagVersion"
+
+npm run build
+
+cd www
+
+git init
+git add -A
+git commit -m "new deployment for release v$tagVersion"
+git push -f git@github.com:alexchristianqr/gmail.git master:gh-pages
+
+cd -
